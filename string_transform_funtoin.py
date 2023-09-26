71f8e045412070e98fbb249eca1080057fa93e14
19G21A04I6 <145839231+19G21A04I6@users.noreply.github.com>
2023-09-26 16:01:48 +0530

    Add files via upload

---
+++ string_transform_funtoin.py
@@ -0,0 +1,143 @@
+#string transform function
+#capitalize
+a="dhahul shaik"
+print(a.capitalize())
+
+#title
+a="dhahul shaik"
+print(a.title())
+
+#upper
+a="dhahul shaik"
+print(a.upper())
+
+
+#lower
+a="Dhahul Shaik"
+print(a.lower())
+
+#casehold
+a="Dhahul Shaik"
+print(a.casefold())
+
+#swapcase
+a="dhahul shaik"
+print(a.swapcase())
+
+#capitalize
+a="123"
+print(a.capitalize())
+
+#title
+a="123"
+print(a.title())
+
+#upper
+a="123"
+print(a.upper())
+
+
+#lower
+a="123"
+print(a.lower())
+
+#casehold
+a="123"
+print(a.casefold())
+
+#swapcase
+a="123"
+print(a.swapcase())
+
+#isalnum
+a="123"
+print(a.isalnum())
+
+#isalpha
+a="123"
+print(a.isalpha())
+
+#isdigit
+a="123"
+print(a.isdigit())
+
+#isascii
+a="123"
+print(a.isascii())
+
+#islower
+a="123"
+print(a.islower())
+
+#isdecimal
+a="123"
+print(a.isdecimal())
+
+#isidentifieer
+a="123"
+print(a.isidentifier())
+
+#isnumeric
+a="123"
+print(a.isnumeric())
+
+#isprintable
+a="123"
+print(a.isprintable())
+
+#isspace
+a="123"
+print(a.isspace())
+
+#istitle
+a="123"
+print(a.istitle())
+
+#isalnum
+a="Dhahul Shaik"
+print(a.isalnum())
+
+#isalpha
+a="Dhahul Shaik"
+print(a.isalpha())
+
+#isdigit
+a="Dhahul Shaik"
+print(a.isdigit())
+
+#isascii
+a="Dhahul Shaik"
+print(a.isascii())
+
+#islower
+a="Dhahul Shaik"
+print(a.islower())
+
+#isdecimal
+a="Dhahul Shaik"
+print(a.isdecimal())
+
+#isidentifieer
+a="Dhahul Shaik"
+print(a.isidentifier())
+
+#isnumeric
+a="Dhahul Shaik"
+print(a.isnumeric())
+
+#isprintable
+a="Dhahul Shaik"
+print(a.isprintable())
+
+#isspace
+a="Dhahul Shaik"
+print(a.isspace())
+
+#istitle
+a="Dhahul Shaik"
+print(a.istitle())
+
+#istitle
+a="12345.8"
+print(a.isdecimal())
+
