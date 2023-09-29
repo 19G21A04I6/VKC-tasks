662f76a41e3714a7aaf3f68027942582d1d10a84
19G21A04I6 <145839231+19G21A04I6@users.noreply.github.com>
2023-09-29 15:47:47 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,156 @@
+#operator
+#Arthematic operator
+#addition
+a = 10
+b = 8
+print(a + b)
+
+#substraction
+a = 10
+b = 8
+print(a - b)
+
+#multiplication
+a = 10
+b = 8
+print(a * b)
+
+#division
+a = 10
+b = 8
+print(a / b)
+
+#floor division
+a = 10
+b = 8
+print(a // b)
+
+#module
+a = 10
+b = 8
+print(a % b)
+
+
+#assighment operators
+#equal
+a = 10
+print(a)
+
+#+=
+a = 10
+a += 2
+print(a)
+
+#-=
+a = 10
+a -= 2
+print(a)
+
+#*=
+a = 10
+a *= 2
+print(a)
+
+#/=
+a = 10
+a /= 2
+print(a)
+
+#%=
+a = 10
+a %= 2
+print(a)
+
+#//=
+a = 10
+a //= 2
+print(a)
+
+
+#Logical operator
+#and
+a = 10
+b = 8
+c = 5
+print(a>b)
+print(a > b and b > a)
+
+
+a = 10
+b = 8
+c = 5
+print(a<b)
+print(a > b and b < a)
+
+#Or
+a = 10
+b = 8
+c = 5
+print(a>b)
+print(a > b or b > a)
+
+a = 10
+b = 8
+c = 5
+print(a<b)
+print(a > b or b < a)
+
+#Not
+a = 10
+b = 8
+c = 5
+print(a>b)
+print(not(a > b or b > a))
+
+a = 10
+b = 8
+c = 5
+print(a<b)
+print(not(a > b or b < a))
+
+#comparision operator
+#==
+a = 10
+b = 10
+print(a==b)
+
+#<=
+a = 10
+b = 10
+print(a<=b)
+
+#>=
+a = 10
+b = 10
+print(a>=b)
+
+#!=
+a = 10
+b = 10
+print(a!=b)
+
+# <
+a = 10
+b = 10
+print(a<b)
+
+# >
+a = 10
+b = 10
+print(a>b)
+
+#bitwise operator
+#bitwise and opertor
+a = 10
+b = 10
+print(a&b)
+
+#bitwise or operator
+a = 10
+b = 10
+print(a | b)
+
+#bitwise xor operator
+a = 10
+b = 10
+print(a^b)
