4d7297075c9c6f927513292aaa5c0574581e91ae
19G21A04I6 <145839231+19G21A04I6@users.noreply.github.com>
2023-09-30 15:25:36 +0530

    operator.py

--- operator.py
+++ operator.py
@@ -154,3 +154,68 @@ print(a | b)
 a = 10
 b = 10
 print(a^b)
+
+
+
+
+#saturday task
+#left shift operator
+a = 10
+b = 3
+print(a>>b)
+
+a = 3
+b = 10
+print(a>>b)
+
+#right shift opertor
+a = 10
+b = 3
+print(a<<b)
+
+a = 3
+b = 10
+print(a<<b)
+
+#check address variable
+a = 10
+print(id(a))
+
+a = 2
+b = 8
+print(id(a))
+print(id(b))
+
+#identify operator
+#is
+a = 2
+b = 8
+print(a is b )
+
+a = 8
+b = 8
+print(b is a)
+
+#is not
+a = 2
+b = 8
+print(a is not b)
+
+a = 8
+b = 8
+print(b is not a)
+
+#member operator
+#in
+a = "Dhahul shaik"
+print( "a" in a)
+
+a = "Dhahul shaik"
+print( "Ali" in a)
+
+#in not
+a = "Dhahul shaik"
+print( "a" not in a)
+
+a = "Dhahul shaik"
+print( "Ali" not in a)
