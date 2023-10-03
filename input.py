dfd1ab45a37c59141452d075e946f99492083d9d
19G21A04I6 <145839231+19G21A04I6@users.noreply.github.com>
2023-10-03 16:04:48 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,203 @@
+#take input in python
+#string
+a = input("Enter the value : ")
+b = input("Enter the value : ")
+print(a +" "+b)
+
+a = input("Enter the value : ")
+b = input("Enter the value : ")
+c = a+" "+b
+print(c.title())
+
+
+a = input("Enter the value : ")
+b = input("Enter the value : ")
+c = a+" "+b
+print(c.upper())
+
+a = input("Enter the value : ")
+b = input("Enter the value : ")
+c = a+" "+b
+print(c.lower())
+
+a = input("Enter the value : ")
+b = input("Enter the value : ")
+c = a+" "+b
+print(c.capitalize())
+
+a = input("Enter the value : ")
+b = input("Enter the value : ")
+c = a+" "+b
+print(c.casefold())
+
+a = input("Enter the value : ")
+b = input("Enter the value : ")
+c = a+" "+b
+print(c.swapcase())
+
+a = input("Enter the value : ")
+b = input("Enter the value : ")
+c = a+" "+b
+print(c.isascii())
+
+a = input("Enter the value : ")
+b = input("Enter the value : ")
+c = a+" "+b
+print(c.isspace())
+
+a = input("Enter the value : ")
+b = input("Enter the value : ")
+c = a+" "+b
+print(c.islower())
+
+a = input("Enter the value : ")
+b = input("Enter the value : ")
+c = a+" "+b
+print(c.isalpha())
+
+a = input("Enter the value : ")
+b = input("Enter the value : ")
+c = a+" "+b
+print(c.istitle())
+
+a = int(input("Enter the value : "))
+b = int(input("Enter the value : "))
+c = a+b
+print(c)
+
+a = input("Enter the value : ")
+b = input("Enter the value : ")
+c = a+" "+b
+print(c.isnumeric())
+
+a = input("Enter the value : ")
+b = input("Enter the value : ")
+c = a+" "+b
+print(c.isdigit())
+
+a = input("Enter the value : ")
+b = input("Enter the value : ")
+c = a+" "+b
+print(c.isalnum())
+
+a = input("Enter the value : ")
+b = input("Enter the value : ")
+c = a+" "+b
+print(c.isdecimal())
+
+a = bin(input("enter the value : "))
+print(c)
+
+a = oct(input("enter the value : "))
+print(c)
+
+a = hex(input("enter the value : "))
+print(c)
+
+a = int(input("enter the value : "))
+print(c)
+
+a = float(input("enter the value : "))
+print(c)
+
+a = complex(input("enter the value : "))
+print(c)
+
+a = float(input("enter the value : "))
+print(c)
+
+a = int(input("Enter the value : "))
+b = int(input("Enter the value : "))
+c = a-b
+print(c)
+
+a = int(input("Enter the value : "))
+b = int(input("Enter the value : "))
+c = a*b
+print(c)
+
+a = int(input("Enter the value : "))
+b = int(input("Enter the value : "))
+c = a/b
+print(c)
+
+a = int(input("Enter the value : "))
+b = int(input("Enter the value : "))
+c = a%b
+print(c)
+
+a = int(input("Enter the value : "))
+b = int(input("Enter the value : "))
+c = a//b
+print(c)
+
+a = int(input("Enter the value : "))
+b = int(input("Enter the value : "))
+c = a==b
+print(c)
+
+a = int(input("Enter the value : "))
+b = int(input("Enter the value : "))
+c = a > b
+print(c)
+
+a = int(input("Enter the value : "))
+b = int(input("Enter the value : "))
+c = a < b
+print(c)
+
+a = int(input("Enter the value : "))
+b = int(input("Enter the value : "))
+c = a >= b
+print(c)
+
+a = int(input("Enter the value : "))
+b = int(input("Enter the value : "))
+c = a <= b
+print(c)
+
+a = int(input("Enter the value : "))
+b = int(input("Enter the value : "))
+c = a > b
+print(c)
+
+a = int(input("Enter the value : "))
+b = int(input("Enter the value : "))
+c = a > b
+print(c)
+
+a = int(input("Enter the value : "))
+b = int(input("Enter the value : "))
+c = int(input("Enter the value : "))
+d = a > b and a > c
+print(d)
+
+a = int(input("Enter the value : "))
+b = int(input("Enter the value : "))
+c = int(input("Enter the value : "))
+d = a > b or a > c
+print(d)
+
+a = int(input("Enter the value : "))
+b = int(input("Enter the value : "))
+c = int(input("Enter the value : "))
+d =not( a > b and a > c)
+print(d)
+
+a = int(input("Enter the value : "))
+b = int(input("Enter the value : "))
+c = a & b
+print(c)
+
+a = int(input("Enter the value : "))
+b = int(input("Enter the value : "))
+c = a | b
+print(c)
+
+a = int(input("Enter the value : "))
+b = int(input("Enter the value : "))
+c = a ^ b
+print(c)
+
+
+
