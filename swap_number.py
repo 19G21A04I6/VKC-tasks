d67e138f29323e02b3648477ba2d1ca91c551f32
19G21A04I6 <145839231+19G21A04I6@users.noreply.github.com>
2023-10-12 23:36:08 +0530

    Add files via upload

---
+++ swap_number.py
@@ -0,0 +1,14 @@
+a = int(input("Enter the number :"))
+b = int(input("Enter the number :"))
+temp =a
+a = b
+b = temp
+print(a ,b)
+
+
+a = int(input("Enter the number :"))
+b = int(input("Enter the number :"))
+a = a + b
+b = a - b
+a = a - b
+print(a ,b)
