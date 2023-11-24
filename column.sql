274dd962c0fbc7038e5db259d5be7306415a57c6
19G21A04I6 <145839231+19G21A04I6@users.noreply.github.com>
2023-11-24 16:52:23 +0530

    Add files via upload

---
+++ column.sql
@@ -0,0 +1,12 @@
+create table zomato.`users`(
+id int,
+username varchar(29),
+email varchar(50),
+street varchar(50),
+location varchar(49),
+city varchar(34),
+state varchar(39),
+country varchar(30),
+pincode int
+);
+
