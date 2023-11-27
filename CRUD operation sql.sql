5e36787fc448d3fa19a1e21a98f3d9649983c43c
19G21A04I6 <145839231+19G21A04I6@users.noreply.github.com>
2023-11-27 13:55:06 +0530

    Update CRUD operation sql.sql

--- CRUD operation sql.sql
+++ CRUD operation sql.sql
@@ -4,3 +4,15 @@ insert into zomato.users(id,username,email,street,location,city,state,country,pi
 	(2,'sai','sai@gamil.com','agraharapet','naidupet','tirupati','AP','india',524126);
     update zomato.users set email='dhahul@gmail.com' where email='d@gmail.com';
 	delete from zomato.users where id=2;
+
+select * from zomato.user1;
+insert into  zomato.user1(id,username)
+		-- value(1,'dhahul');
+        value(1,'shaik');
+select * from zomato.user2;
+insert into  zomato.user2(id,username)
+	value(null,'dhahul');
+select * from zomato.user3;
+insert into  zomato.user3(id,username)
+	value(1,'dhahul')
+    value(null,'dhahul')
